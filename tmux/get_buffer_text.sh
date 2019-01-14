9eac9111b6b20aeb307ef1a502bdb45b87fffbb6
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2019-01-15 00:35:57 +0900

    バッファリミットを超えたテキストに対応

--- tmux/get_buffer_text.sh
+++ tmux/get_buffer_text.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 set -eu
 
-tmux new-window "\
-  cat ~/.zsh/$1; \
-  tmux capture-pane -p -S -1000 \
-    | strutil newline -t=1 > ~/sample.txt \
-"
+readonly OUTPUT_FILE=~/sample.txt
+rm -f ${OUTPUT_FILE}
+
+readonly SPLIT_N=$((($(cat ~/.zsh/$1 | wc -l)-1) / 50000 + 1))
+if [[ $SPLIT_N -gt 1 ]]; then
+  readonly SESSION_N=$(tmux list-sessions | wc -l)
+  for x in $(seq 0 $(($SPLIT_N - 1))); do
+    tmux new-window "\
+      cat ~/.zsh/$1 | \
+        strutil line -0 $(($x*50000)):$((($x+1)*50000)); \
+      tmux capture -t '$(($x+$SESSION_N)).0' -p -S - | \
+        strutil newline -t=1 \
+        > ${OUTPUT_FILE}.${x}; \
+    "
+    if [[ $x -gt 9 ]]; then
+      break
+    fi
+  done
+else
+  tmux capture-pane -p -S - | strutil newline -t=1 > ${OUTPUT_FILE}
+fi
 
