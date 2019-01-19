68d55f744347064553250afe354beba1cf6d61f1
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2019-01-20 02:22:19 +0900

    add remove_escapesequence

---
+++ zsh/remove_escapesequence.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+#my $SOP = "[START_OF_PROMPT]";
+my $SOP = "";
+
+while (<>) {
+    s/[\r\n]//g;
+
+    s/\[3m%\[23m/[3m${SOP}[23m/g;
+
+    while (/^(.*?)(+)(.*)$/) {
+        my ($head, $space, $tail) = ($1, $2, $3);
+        $head = substr($head, 0, length($head)-length($space));
+        $_ = $head . $tail
+    }
+
+    while (/^(.*?)\[(\d+)D +\[(\d+)D(.*)$/) {
+        my ($head, $space, $tail) = ($1, $2, $4);
+        $head = substr($head, 0, length($head)-$space);
+        $_ = $head . $tail
+    }
+
+    s/\[J/\n/g;
+
+    s/\[\?2004[hl]//g;
+
+    print $_ . "\n";
+}
+
