6d932ecb3ba622cfbc90d637d6ff0c63aa991bd8
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2019-01-23 21:27:37 +0900

    remove_escapesequenceとfzf_buffer_historyを分離

--- zsh/fzf_buffer_history.sh
+++ zsh/fzf_buffer_history.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -eu
 
+readonly START_OF_PROMPT="___START_OF_PROMPT___"
+
 cat $1 | \
-  remove_escapesequence | \
+  remove_escapesequence -s ${START_OF_PROMPT} | \
   tr '\n' '' | \
-  sed -e 's/___START_OF_PROMPT___/\n/g' | \
+  sed -e "s/${START_OF_PROMPT}/\n/g" | \
   fzf --ansi --preview="echo {} | sed -e 's//\n/g' | strutil newline -l=0 -t=1" --preview-window='wrap' | \
   sed -e 's//\n/g' | \
   strutil newline -l=0 -t=1
