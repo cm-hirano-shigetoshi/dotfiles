27ad7c2bef56bcec6462b4c71c9050cfa4267f3f
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2019-01-24 10:41:31 +0900

    コマンドの経過時間を表示

--- zsh/pipestatus.zsh
+++ zsh/pipestatus.zsh
@@ -1,19 +1,23 @@
+PRE_TIME=""
 PRE_COMMAND=""
 
 function preexec_pipestatus() {
+    PRE_TIME=$(date +%s)
     PRE_COMMAND=$1
 }
 
 function precmd_pipestatus() {
+    local cur_time=$(date +%s)
     PIPESTATUS=$1
     if [ ! -z $PRE_COMMAND ]; then
+        local duration=$(($cur_time - $PRE_TIME))
         STATUS=$(perl $dotfiles/zsh/lib/returnStatus.pl $PIPESTATUS)
         if [ $STATUS -eq 0 ]; then
-            echo "\n@END [${PRE_COMMAND}]"
+          echo "\n[${PRE_COMMAND}] (${duration}s)"
         elif [ $STATUS -eq 1 ]; then
-            echo "\n[36m@END [${PRE_COMMAND}]=>[${PIPESTATUS}][0m"
+            echo "\n[36m[${PRE_COMMAND}]=>[${PIPESTATUS}] (${duration}s)[0m"
         else
-            echo "\n[31m@END [${PRE_COMMAND}]=>[${PIPESTATUS}][0m"
+            echo "\n[31m[${PRE_COMMAND}]=>[${PIPESTATUS}] (${duration}s)[0m"
         fi
         PROMPT=$PROMPT_BASE
     fi
