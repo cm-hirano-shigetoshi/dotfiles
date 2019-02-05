4637996b7cfd1467a34b362ea9863603b78f4f28
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2019-02-05 11:03:34 +0900

    最後は素直にSequenceを処理

--- zsh/remove_escapesequence_2.pl
+++ zsh/remove_escapesequence_2.pl
@@ -5,25 +5,50 @@ use warnings;
 while (<>) {
     s/[\r\n]//g;
     s/\[3m%\[23m/[3m[23m/g;
-
-    while (/^(.*?)(+)(.*)$/) {
-        my ($head, $space, $tail) = ($1, $2, $3);
-        $head =~ s/\[\d+m//g;
-        $head = substr($head, 0, length($head)-length($space));
-        $_ = $head . $tail
-    }
-
-    while (/^(.*?)\[(\d+)D(.*)$/) {
-        my ($head, $delete, $tail) = ($1, $2, $3);
-        $head =~ s/\[\d+m//g;
-        $head = substr($head, 0, length($head)-$delete);
-        $_ = $head . $tail
-    }
-
     s/\[J/\n/g;
-
     s/\[\?2004[hl]//g;
 
+    $_ = &seq($_);
+
     print $_ . "\n";
+}
+
+sub seq {
+    my @buf = ();
+    my $pos = 0;
+    my @chars = split(//, $_[0]);
+    for (my $i=0; $i<scalar(@chars); $i++) {
+        my $c = $chars[$i];
+        if ($c eq "") {
+            $pos--;
+        } elsif ($c eq "") {
+            if ($chars[$i+1] eq "[") {
+                my $j = 2;
+                my $num = "";
+                for (; $chars[$i+$j] =~ /[0-9;]/; $j++) {
+                    $num .= $chars[$i+$j];
+                }
+                my $alpha = $chars[$i+$j];
+                $i += $j;
+                #print STDERR "\\e[$num$alpha";
+
+                if ($alpha eq "C") {
+                    $pos += $num;
+                } elsif ($alpha eq "D") {
+                    $pos -= $num;
+                } else {
+                    # do nothing.
+                }
+            }
+        } else {
+            if ($pos <= $#buf) {
+                $buf[$pos++] = $c;
+            } else {
+                push(@buf, $c);
+                $pos++;
+            }
+        }
+    }
+    return join("", @buf);
 }
 
