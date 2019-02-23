22dff32e905b822b48d1bd08cd1c18cbbdeea223
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2019-02-24 02:37:42 +0900

    微修正

--- zsh/completion-with-this-screen.zsh
+++ zsh/completion-with-this-screen.zsh
@@ -1,11 +1,11 @@
 if [[ ! -z $TMUX ]]; then
   function __fzf_screen_buffer() {
-    cat ~/.zsh/buffer_history/$$ | remove_escapesequence > ~/.screen_buffer
+    head -n -2 ~/.zsh/buffer_history/$$ | remove_escapesequence > ~/.screen_buffer
     LBUFFER+=$(strutil tokenize ~/.screen_buffer \
       | grep -v '^\s*$' \
       | tac \
       | strutil unique \
-      | fzf -e -q "$BUFFER" -1 \
+      | fzf -e -q "$BUFFER" -1 -0 \
     )
   }
   zle -N __fzf_screen_buffer
