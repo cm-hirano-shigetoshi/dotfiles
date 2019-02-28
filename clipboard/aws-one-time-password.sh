29765dc9b864cdb45d196cd7dd5e71ed39784b40
Shigetoshi Hirano <hirano.shigetoshi@classmethod.jp>
2019-02-28 18:18:29 +0900

    1passwordの認証切れに対応

--- clipboard/aws-one-time-password.sh
+++ clipboard/aws-one-time-password.sh
@@ -1,4 +1,15 @@
+#!/bin/bash
 pass=$(OP_SESSION_classmethod=$(cat ~/.1password) op get totp aws)
+if [[ -z "${pass}" ]]; then
+  tmux split-window 'op signin classmethod --output=raw > ~/.1password'
+  while pgrep -f 'op signin ' >/dev/null; do
+    sleep 1
+  done
+  if [[ ! -s ~/.1password ]]; then
+    exit
+  fi
+  pass=$(OP_SESSION_classmethod=$(cat ~/.1password) op get totp aws)
+fi
 
 if [[ $# -gt 0 ]]; then
   readonly PANE_ID=$1
@@ -6,4 +17,3 @@ if [[ $# -gt 0 ]]; then
 else
   echo "${pass}"
 fi
-
