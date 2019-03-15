b911e4a4caf1592b91b22aa356ef435fe720cd68
Shigetoshi Hirano <hirano.shigetoshi@classmethod.jp>
2019-03-15 22:17:08 +0900

    プレビューは折り返し

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,5 +1,5 @@
 FZF_DEFAULT_OPTS=""
-FZF_DEFAULT_OPTS+=" --exact --no-mouse --ansi --preview-window=up"
+FZF_DEFAULT_OPTS+=" --exact --no-mouse --ansi --preview-window=up:wrap"
 FZF_DEFAULT_OPTS+=" --bind='up:preview-up,down:preview-down,alt-k:preview-page-up,alt-j:preview-page-down,alt-p:toggle-preview,alt-c:execute-silent(echo -n {} | pbcopy)'"
 FZF_DEFAULT_OPTS+=" --preview='echo {}'"
 export FZF_DEFAULT_OPTS
