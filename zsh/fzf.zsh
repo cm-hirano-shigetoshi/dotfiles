35ee22d2c1f468653550d84a9621462e13b27dd5
Shigetoshi Hirano <hirano.shigetoshi@classmethod.jp>
2019-03-28 21:53:08 +0900

    fzfのキーバインドを変更

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,6 +1,6 @@
 FZF_DEFAULT_OPTS=""
 FZF_DEFAULT_OPTS+=" --exact --no-mouse --ansi --preview-window=up:wrap"
-FZF_DEFAULT_OPTS+=" --bind='up:preview-up,down:preview-down,alt-k:preview-page-up,alt-j:preview-page-down,alt-p:toggle-preview,alt-c:execute-silent(echo -n {} | pbcopy)'"
+FZF_DEFAULT_OPTS+=" --bind='shift-up:preview-up,shift-down:preview-down,alt-up:preview-page-up,alt-down:preview-page-down,alt-p:toggle-preview,alt-c:execute-silent(echo -n {} | pbcopy)'"
 FZF_DEFAULT_OPTS+=" --preview='echo {}'"
 export FZF_DEFAULT_OPTS
 
