bb542aff9b37877b2f65e1ceb0c5646ca6153625
Shigetoshi Hirano <hirano.shigetoshi@classmethod.jp>
2019-06-16 14:27:15 +0900

    v.zshを廃止

--- zsh/v.zsh
+++
@@ -1,11 +0,0 @@
-function v() {
-  local out
-  out=$(fzfer run ~/dotfiles/zsh/fzfer/v.yml)
-  if [[ -n "$out" ]]; then
-    BUFFER="vim $out"
-    zle accept-line
-  fi
-}
-zle -N v
-#bindkey "v" v
-
