baffe652ee7fd913409905dfcc8fa3229d1c302b
Shigetoshi Hirano <hirano.shigetoshi@classmethod.jp>
2019-10-10 13:30:40 +0900

    1行の時の改行を削除

--- clipboard/clipboard.sh
+++ clipboard/clipboard.sh
@@ -1,9 +1,9 @@
 function fzf_history() {
   export PATH="~/local/bin:${PATH}"
   local readonly CLIPBOARD_HISTORY_FILE="${HOME}/.clipboard_history"
-  local readonly YAML_FILE="${HOME}/dotfiles/clipboard/clipboard.yml"
+  local readonly TOOL_DIR="${HOME}/dotfiles/clipboard"
   local result
-  result=$(fzfyml run ${YAML_FILE})
+  result=$(fzfyml run ${TOOL_DIR}/clipboard.yml)
   if [[ -z "${result}" ]]; then
     return
   fi
@@ -12,12 +12,14 @@ function fzf_history() {
   if [[ "${type}" = "clipboard" ]]; then
     sed '1d' <<< "${result}" \
       | tr '' '\n' \
+      | perl ${TOOL_DIR}/del_newline.pl \
       | pbcopy
   elif [[ "${type}" = "edit" ]]; then
     local temp_file
     temp_file=$(mktemp "/tmp/clipboard_history.XXXXXX")
     sed '1d' <<< "${result}" \
       | tr '' '\n' \
+      | perl ${TOOL_DIR}/del_newline.pl \
       > ${temp_file}
     ${EDITOR-vim} ${temp_file}
     if [[ -s ${temp_file} ]]; then
