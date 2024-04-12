97eae608c90319119010e782782993525d6a3766
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2024-04-13 04:45:05 +0900

    coreutilsのインストール

--- mac/install.sh
+++ mac/install.sh
@@ -31,3 +31,8 @@ ln -sf $SCRIPT_DIR/zsh/zprofile $HOME/.zprofile
 ln -sf $SCRIPT_DIR/zsh/zshenv $HOME/.zshenv
 ln -sf $SCRIPT_DIR/zsh/zshrc $HOME/.zshrc
 ln -sf $SCRIPT_DIR/zsh/p10k.zsh $HOME/.config/zsh/p10k.zsh
+
+#
+# coreutils
+#
+brew install coreutils gnu-sed
