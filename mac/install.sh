4e2d3aa1c8dc2207999c8af5a2c05c6b0702b811
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2024-04-13 00:35:38 +0900

    zshのインストール

--- mac/install.sh
+++ mac/install.sh
@@ -9,4 +9,12 @@ SCRIPT_DIR=$(perl -MCwd=realpath -le 'print realpath shift' "$0/..")
 mkdir -p $HOME/.config/wezterm
 ln -sf $SCRIPT_DIR/wezterm/wezterm.lua $HOME/.config/wezterm/wezterm.lua
 
-
+#
+# zsh
+#
+echo 'export ZDOTDIR=$HOME/.config/zsh' > $HOME/.zshenv
+mkdir -p $HOME/.config/zsh
+ln -sf $SCRIPT_DIR/zsh/zprofile $HOME/.config/zsh/zprofile
+ln -sf $SCRIPT_DIR/zsh/zshenv $HOME/.config/zsh/zshenv
+ln -sf $SCRIPT_DIR/zsh/zshrc $HOME/.config/zsh/zshrc
+ln -sf $SCRIPT_DIR/zsh/p10k.zsh $HOME/.config/zsh/p10k.zsh
