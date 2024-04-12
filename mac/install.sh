a3ac5f38e0cdb8d061a0158b54cb40ce9449e14a
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2024-04-13 04:30:05 +0900

    RustとHomebrew

--- mac/install.sh
+++ mac/install.sh
@@ -4,6 +4,20 @@ set -euo pipefail
 SCRIPT_DIR=$(perl -MCwd=realpath -le 'print realpath shift' "$0/..")
 
 #
+# Rust
+#
+if ! builtin which cargo; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
+
+#
+# Homebrew
+#
+if ! builtin which brew; then
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+#
 # WezTerm
 #
 mkdir -p $HOME/.config/wezterm
