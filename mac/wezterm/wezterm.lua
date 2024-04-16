e8acbee4b394ddd6b5af3751ae12e3826c4e2064
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2024-04-17 01:34:54 +0900

    カラースキーマ変更

--- mac/wezterm/wezterm.lua
+++ mac/wezterm/wezterm.lua
@@ -16,7 +16,7 @@ end
 config.font = wezterm.font_with_fallback({family="HackGen", weight="Regular", stretch="Normal", style="Normal"})
 config.font_size = 13.0
 config.window_background_opacity = 0.80
-config.color_scheme = 'Shic (terminal.sexy)'
+config.color_scheme = 'Dracula'
 
 config.keys = {
     {
